5a82a8d11581e941d08f0e4efc49ad65be031b8e
ASUS <abhiraskar@gmail.com>
2023-08-30 14:20:52 +0530

    Searching Code Correction

--- Routes/Shelter.routes.js
+++ Routes/Shelter.routes.js
@@ -4,26 +4,28 @@ const ShelterRouter = express.Router();
 
 ShelterRouter.get("/", async (req, res) => {
   try {
-    const { order, location, page, limit, search } = req.query;
+    const { order, location, page, name } = req.query;
 
     let query = {};
     let sortby = {};
 
     if (order == "asc") {
       sortby.price = 1;
-    } else if (order == "desc") {
-      sortby.price = -1
-    } else {
-      sortby = null
+    }
+    else if (order == "desc") {
+      sortby.price = -1;
+    }
+    else {
+      sortby = null;
     }
 
-    // if (type) {
-    //   query.type = type.toLowerCase();
-    // }
+    if (name) {
+      query.name = ({ $regex: name, $options: "i" })
+    }
+
     if (location) {
       query.location = location;
     }
-
 
     let currentPage = 1;
     let sheltersPerPage = 8;
@@ -32,39 +34,22 @@ ShelterRouter.get("/", async (req, res) => {
       currentPage = +page;
     }
 
-    const shelters = await ShelterModel.find(query).sort(sortby)
+    const shelters = await ShelterModel.find(query)
+      .sort(sortby)
       .skip((currentPage - 1) * sheltersPerPage)
       .limit(sheltersPerPage);
 
     res.status(200).json({
       shelters,
       totalData: await ShelterModel.countDocuments(query),
-      totalPages: Math.ceil(await ShelterModel.countDocuments(query) / sheltersPerPage),
+      totalPages: Math.ceil(
+        (await ShelterModel.countDocuments(query)) / sheltersPerPage
+      ),
       currentPage,
     });
-
-
-    // const Shelters = await ShelterModel.find(query);
-
-
-
-    // Searching Data
-    // if (search) {
-    //   const searchQuery = search.toLowerCase();
-
-    //   Shelters = Shelters.filter(
-    //     (pet) =>
-    //       pet.name.toLowerCase().includes(searchQuery) ||
-    //       pet.type.toLowerCase().includes(searchQuery) ||
-    //       pet.location.toLowerCase().includes(searchQuery) ||
-    //       pet.price.includes(searchQuery)
-    //   );
-    // }
-
-
   } catch (error) {
     res.status(400).json({ error: error.message });
-    console.log(error)
+    console.log(error);
   }
 });
 
