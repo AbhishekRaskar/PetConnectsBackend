dfaa8ac427b4f8cf73df687c2bc3b4e5d81a4e23
ASUS <abhiraskar@gmail.com>
2023-08-30 14:07:56 +0530

    Sorting Code Correction

--- Routes/Shelter.routes.js
+++ Routes/Shelter.routes.js
@@ -4,31 +4,25 @@ const ShelterRouter = express.Router();
 
 ShelterRouter.get("/", async (req, res) => {
   try {
-    const { type, sortBy, sortOrder, location, page, limit, search } = req.query;
+    const { order, location, page, limit, search } = req.query;
 
     let query = {};
+    let sortby = {};
 
-    if (type) {
-      query.type = type.toLowerCase();
+    if (order == "asc") {
+      sortby.price = 1;
+    } else if (order == "desc") {
+      sortby.price = -1
+    } else {
+      sortby = null
     }
+
+    // if (type) {
+    //   query.type = type.toLowerCase();
+    // }
     if (location) {
       query.location = location;
     }
-
-
-    // Paginating data
-    // const currentPage = parseInt(page) || 1;
-    // const limitPerPage = parseInt(limit) || 10;
-    // const startIndex = (currentPage - 1) * limitPerPage;
-    // const endIndex = currentPage * limitPerPage;
-    // const paginatedData = Shelters.slice(startIndex, endIndex);
-
-    // res.status(200).json({
-    //   data: paginatedData,
-    //   totalData: Shelters.length,
-    //   totalPages: Math.ceil(Shelters.length / limitPerPage),
-    //   currentPage,
-    // });
 
 
     let currentPage = 1;
@@ -38,7 +32,7 @@ ShelterRouter.get("/", async (req, res) => {
       currentPage = +page;
     }
 
-    const shelters = await ShelterModel.find(query)
+    const shelters = await ShelterModel.find(query).sort(sortby)
       .skip((currentPage - 1) * sheltersPerPage)
       .limit(sheltersPerPage);
 
@@ -50,26 +44,22 @@ ShelterRouter.get("/", async (req, res) => {
     });
 
 
-    const Shelters = await ShelterModel.find(query);
+    // const Shelters = await ShelterModel.find(query);
 
-    // Sorting the Data
-    if (sortBy) {
-      const order = sortOrder === "desc" ? -1 : 1;
-      Shelters.sort((a, b) => (a[sortBy] > b[sortBy] ? order : -order));
-    }
+
 
     // Searching Data
-    if (search) {
-      const searchQuery = search.toLowerCase();
+    // if (search) {
+    //   const searchQuery = search.toLowerCase();
 
-      Shelters = Shelters.filter(
-        (pet) =>
-          pet.name.toLowerCase().includes(searchQuery) ||
-          pet.type.toLowerCase().includes(searchQuery) ||
-          pet.location.toLowerCase().includes(searchQuery) ||
-          pet.price.includes(searchQuery)
-      );
-    }
+    //   Shelters = Shelters.filter(
+    //     (pet) =>
+    //       pet.name.toLowerCase().includes(searchQuery) ||
+    //       pet.type.toLowerCase().includes(searchQuery) ||
+    //       pet.location.toLowerCase().includes(searchQuery) ||
+    //       pet.price.includes(searchQuery)
+    //   );
+    // }
 
 
   } catch (error) {
